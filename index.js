f7f8b246f362e30f17cb1f1ebfc13c3a12859b24
Rotsy Rakotondrainy <rakotondrainyrotsy@gmail.com>
2025-12-26 21:00:40 +0300

    Add multiplication function and update module exports

--- index.js
+++ index.js
@@ -6,13 +6,19 @@ function addition(a, b) {
   return a + b;
 }
 
+function multiplication(a, b) {
+    return a * b;
+}
+
 module.exports = {
   saluer,
-  addition
+  addition,
+  multiplication
 };
 
 if (require.main === module) {
   console.log(saluer('Monde'));
   console.log(`Résultat de 5 + 3 = ${addition(5, 3)}`);
+  console.log(`Résultat de 5 * 3 = ${multiplication(5, 3)}`);
 }
 
